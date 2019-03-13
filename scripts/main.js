1df9bfd38fe9b0b30f65804cd7068ac9b5f58cab
Michael McKay <mamckay89@gmail.com>
2019-03-12 23:01:13 -0600

    working on footer with alpha layer image

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,18 @@
 function setup() {
     $(document).ready(function () {
-        $('.portfolio-button').on('click', event => {
-            document.getElementById('portfolio-station').scrollIntoView({
-                behavior: 'smooth'
-            });
-            event.preventDefault();
-        });
+        // $('.portfolio-button').on('click', event => {
+        //     document.getElementById('portfolio-station').scrollIntoView({
+        //         behavior: 'smooth'
+        //     });
+        //     event.preventDefault();
+        // });
 
+        $(document).ready(function () {
+            $('.portfolio-button').on('click', event => {
+                window.scrollTo(0, window.screen.height);
+                event.preventDefault();
+            });
+        });
 
     });
 }
