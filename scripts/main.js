a66537d4e7b6b274d9a20896c924a546f028d0ea
Michael McKay <mamckay89@gmail.com>
2019-03-14 18:32:58 -0600

    css changes to landing page

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,7 @@ function setup() {
                 console.log(document.getElementById('portfolio-button').value);
 
                 if (document.getElementById('portfolio-button').value === 'skills') {
-                    window.scrollTo(0, window.screen.availHeight - 100);
+                    window.scrollTo(0, window.screen.height - 100);
                     event.preventDefault();
                 }
                 else if (document.getElementById('portfolio-button').value === 'portfolio') {
