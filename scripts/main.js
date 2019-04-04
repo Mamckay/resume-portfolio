0cbb513a05665c7c8022e803bb500d1f5eb87177
Michael McKay <mamckay89@gmail.com>
2019-04-04 04:50:12 -0600

    working on navbar

--- scripts/main.js
+++ scripts/main.js
@@ -8,26 +8,43 @@ function setup() {
         // });
 
         $(document).ready(function () {
-            $('#portfolio-button').on('change', event => {
-                console.log(document.getElementById('portfolio-button').value);
+            $('#portfolio-button').on('click', event => {
+                console.log(document.getElementById("nav-link").style.display);
 
-                if (document.getElementById('portfolio-button').value === 'skills') {
-                    window.scrollTo(0, $('.skills-title').position().top - 50);
-                    event.preventDefault();
+                if (document.getElementById("nav-link").style.display === "flex") {
+                    document.getElementById("nav-link").style.display = "none";
+                } else {
+                    document.getElementById("nav-link").style.display = "flex";
+                    document.getElementById("nav-link").style.flexDirection = "column";
+                    document.getElementById("nav-link").style.position = "fixed";
                 }
-                else if (document.getElementById('portfolio-button').value === 'portfolio') {
-                    window.scrollTo(0, $('.portfolio-title').position().top - 50);
-                    event.preventDefault();
-                    $
-                    // document.getElementById('portfolio-station').scrollIntoView({
-                    //     behavior: 'smooth'
-                    // });
-                }
-                else if (document.getElementById('portfolio-button').value === 'home') {
-                    window.scrollTo(0, 0);
-                    event.preventDefault();
-                }
+
+                // if (document.getElementById('portfolio-button').value === 'skills') {
+                //     window.scrollTo(0, $('.skills-title').position().top - 50);
+                //     event.preventDefault();
+                // }
+                // else if (document.getElementById('portfolio-button').value === 'portfolio') {
+                //     window.scrollTo(0, $('.portfolio-title').position().top - 50);
+                //     event.preventDefault();
+                //     $
+                //     // document.getElementById('portfolio-station').scrollIntoView({
+                //     //     behavior: 'smooth'
+                //     // });
+                // }
+                // else if (document.getElementById('portfolio-button').value === 'home') {
+                //     window.scrollTo(0, 0);
+                //     event.preventDefault();
+                // }
             });
+            $('#home').on('click', event => {
+                window.scrollTo(0, 0);
+            })
+            $('#skills').on('click', event => {
+                window.scrollTo(0, $('.skills-title').position().top - 50);
+            })
+            $('#portfolio').on('click', event => {
+                window.scrollTo(0, $('.portfolio-title').position().top - 50);
+            })
         });
 
 
